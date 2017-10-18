4cee59b9d457317c6676dbf96c24fbeb008a44fa
Guilherme Costa <guilherme@bartdigital.com.br>
2017-10-17 23:13:19 -0200

    form

---
+++ app/helpers/Util.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: guilherme
+ * Date: 07/10/17
+ * Time: 21:53
+ */
+
+
+
+
+class Util
+{
+    /**
+     * Formata de 00/00/0000 para 0000-00-00
+     * @param $date
+     * @return string
+     */
+    static function formatDate($date){
+        if($date !== '') {
+            $date = explode('/', $date);
+            return $date[2] . '-' . $date[1] . '-' . $date[0];
+        }else{
+            return '1970-01-01';
+        }
+    }
+
+    /**
+     * Retorna apenas numeros
+     * @param $text
+     * @return string
+     */
+    static function onlyNumbers($text){
+        $text = str_replace('.', '', $text);
+        $text = str_replace('/', '', $text);
+        $text = str_replace('-', '', $text);
+        return $text;
+    }
+
+
+}
