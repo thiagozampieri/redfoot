c4dadb174b14ab1f949756c6aced3f265b02dfb3
Guilherme Costa <guilherme@bartdigital.com.br>
2017-10-17 23:23:23 -0200

    ajuste icon

--- partials/common/footer.php
+++ partials/common/footer.php
@@ -11,8 +11,8 @@
                         <br/>
                         <a href="https://www.facebook.com/RedfootStartups/" title="Facebook"  target="_blank" ><i class="fa fa-facebook-square"></i></a>
                         <a href="https://www.facebook.com/groups/redfoot/?ref=br_rs" title="Facebook Group"  target="_blank" ><i class="fa fa-facebook"></i></a>
-                        <a href="https://www.instagram.com/redfootbrazil/"  target="_blank" title="Instagram" ><i class="fa fa-whatsapp"></i></a>
-                        <a href="https://chat.whatsapp.com/9X20O6HohvdDOpfFsq8qHZ" title="Whatsapp"  target="_blank" ><i class="fa fa-instagram"></i></a>
+                        <a href="https://www.instagram.com/redfootbrazil/"  target="_blank" title="Instagram" ><i class="fa fa-instagram"></i></a>
+                        <a href="https://chat.whatsapp.com/9X20O6HohvdDOpfFsq8qHZ" title="Whatsapp"  target="_blank" ><i class="fa fa-whatsapp"></i></a>
 
 
                     </div>
