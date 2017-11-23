42f10689a101db25e70ee6e7238d35f35e17a09d
thiagobart <thiago@bartdigital.com.br>
2017-11-23 20:53:41 -0200

    agenda

--- partials/index.php
+++ partials/index.php
@@ -112,7 +112,7 @@ $ic = new IndexController();
 <div style="margin: 20px 0px 20px 0px;">
     <div class="container">
         <ul class="row list-group">
-            <? foreach ($ic->getEvents() as $event) { $start_time = new DateTime($event->start_time);?>
+            <?php foreach ($ic->getEvents() as $event) { $start_time = new DateTime($event->start_time);?>
             <li class="row col-sm-12 list-group-item">
                 <div class="row col-sm-12 inline-block"><span class="col-sm-12"><label><?=$start_time->format('d')?></label>/<label class="red"><?=$start_time->format('m')?></label>/<label><?=$start_time->format('Y')?></label> <small class="red"><?=$start_time->format('H:i')?>h</small></span></div>
                 <div class="row col-sm-12">
