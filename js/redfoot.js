a946217637195089a67f617ea177c01ae75fbb7a
thiagobart <thiago@bartdigital.com.br>
2017-11-23 18:51:19 -0200

    quick fix

--- js/redfoot.js
+++ js/redfoot.js
@@ -59,7 +59,7 @@ var initMap = function(map_id) {
 
      //(optional) restore the zoom level after the map is done scaling
      var listener = google.maps.event.addListener(map, "idle", function () {
-     map.setZoom(13);
+     map.setZoom(6);
      google.maps.event.removeListener(listener);
      });
 
