d5fa66c251e6c4195026ea44df62a527b8ef7749
thiagobart <thiago@bartdigital.com.br>
2018-03-26 21:23:16 -0300

    form

--- partials/startup/register.php
+++ partials/startup/register.php
@@ -16,11 +16,12 @@ if (isset($_POST['action']) && $_POST['action'] != '') {
 
 ?>
 
+<script type="text/javascript" src="<?=Config::getUrlBase()?>bower_components/jquery-validation/dist/jquery.validate.js"></script>
 <div id="fh5co-startup-register">
     <div id="contanier" class="startup-register-form background-red">
         <?php Message::showMessage(); ?>
 
-        <form class="col-sm-12 register-form center-align" enctype="multipart/form-data" method="POST" action="" novalidate>
+        <form class="col-sm-12 register-form center-align" id="myForm" enctype="multipart/form-data" method="POST" action="" novalidate>
             <input type="hidden" name="action" value="save"/>
 
 
@@ -482,11 +483,34 @@ if (isset($_POST['action']) && $_POST['action'] != '') {
 </div>
 
 <script type="text/javascript">
+    jQuery.extend(jQuery.validator.messages, {
+        required: "Este campo &eacute; requerido.",
+        remote: "Por favor, corrija este campo.",
+        email: "Por favor, forne&ccedil;a um endere&ccedil;o eletr&ocirc;nico v&aacute;lido.",
+        url: "Por favor, forne&ccedil;a uma URL v&aacute;lida.",
+        date: "Por favor, forne&ccedil;a uma data v&aacute;lida.",
+        dateISO: "Por favor, forne&ccedil;a uma data v&aacute;lida (ISO).",
+        number: "Por favor, forne&ccedil;a um n&uacute;mero v&aacute;lido.",
+        digits: "Por favor, forne&ccedil;a somente d&iacute;gitos.",
+        creditcard: "Por favor, forne&ccedil;a um cart&atilde;o de cr&eacute;dito v&aacute;lido.",
+        equalTo: "Por favor, forne&ccedil;a o mesmo valor novamente.",
+        accept: "Por favor, forne&ccedil;a um valor com uma extens&atilde;o v&aacute;lida.",
+        maxlength: jQuery.validator.format("Por favor, forne&ccedil;a n&atilde;o mais que {0} caracteres."),
+        minlength: jQuery.validator.format("Por favor, forne&ccedil;a ao menos {0} caracteres."),
+        rangelength: jQuery.validator.format("Por favor, forne&ccedil;a um valor entre {0} e {1} caracteres de comprimento."),
+        range: jQuery.validator.format("Por favor, forne&ccedil;a um valor entre {0} e {1}."),
+        max: jQuery.validator.format("Por favor, forne&ccedil;a um valor menor ou igual a {0}."),
+        min: jQuery.validator.format("Por favor, forne&ccedil;a um valor maior ou igual a {0}.")
+    });
+
+    $("#myForm").validate({
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
 
     var last_valid_selection = null;
     $(document).ready(function () {
-
-
         $('#is_formalized').change(function () {
             if ($(this).val() == 1) {
                 $('#only_formalized').show();
